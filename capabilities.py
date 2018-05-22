501a26590e1c344ccdab7ea0f9431630cc12e998
Martino Mensio <martinomensio@outlook.it>
2018-05-22 18:24:05 +0100

    define classes for handling in more structured way, updated examples and started deeper taxonomy

--- capabilities.py
+++ capabilities.py
@@ -1,92 +1,85 @@
+import copy
 from concepts import Context
+from concepts.lattices import Concept
 
-context = Context.fromfile('table_b.csv', frmat='csv')
-lattice = context.lattice
-#lattice.graphviz('normal.png', '.', False, True)
+class LatticeSpecification(object):
+    """A wrapper for concepts and lattices, that based on a table (rows=frames, columns=required capabilities) """
+    
+    def __init__(self, csv_location):
+        # the Frame-capability lattice
+        self.context = Context.fromfile(csv_location, frmat='csv')
+        self.lattice = self.context.lattice
+        # the Frame-uncapability lattice
+        self.context_inv = Context(*self.context.definition().inverted())
+        self.lattice_inv = self.context_inv.lattice
+        # the list of all capabilities and frames
+        self.capabilities = self.context.properties
+        self.frames = self.context.objects
 
-true_capabilities = ['perception', 'navigation', 'learning', 'verbal_interaction', 'information_management']
-true_capabilities = ['information_management', 'navigation', 'moving', 'perception', 'learning']
-#true_capabilities = ['perception']
-
-# upside down
-context_inv = Context(*context.definition().inverted())
-lattice_inv = context_inv.lattice
-
-all_properties = context.properties
-all_objects = context.objects
-
-def get_candidates(frame_name):
-    anticoncept = lattice_inv[frame_name,]
-    all_properties = context.properties
-    #print(anticoncept)
-    to_consider = anticoncept.lower_neighbors
-    results = set()
-    while to_consider:
-        #print(to_consider)
-        print('looping')
-        new_to_consider = []
-        for candidate_link in to_consider:
-            simpler_actions, not_requires = candidate_link
-            #print(simpler_actions, not_requires)
-            requires = [p for p in all_properties if p not in not_requires]
-            if all(elem in true_capabilities for elem in requires):
-                print('can do', simpler_actions)
-                results.update(simpler_actions)
-            new_to_consider.extend(candidate_link.lower_neighbors)
+    def visualize(self, inverse=False, **kvargs):
+        """Call the lattice visualization. 
         
-        to_consider = [candidate for candidate in new_to_consider if candidate is not lattice_inv.infimum]
-        
-    print(results)
+        inverse=True: creates the Frame-capability visualization
+        inverse=False: creates the Frame-uncapability visualization
+        kwargs: put there all filename,directory,render,view stuff"""
+        if inverse:
+            return self.lattice_inv.graphviz(**kvargs)
+        else:
+            return self.lattice.graphviz(**kvargs)
 
-def better(frame_name):
-    # what are the abilities needed
-    wanted = context.intension([frame_name])
-    # its lattice representation
-    anticoncept = lattice_inv[frame_name,]
-    # what cannot be provided, but was asked for
-    cannot_provide = [cap for cap in wanted if cap not in true_capabilities]
-    print('things required that cannot be provided: ', cannot_provide)
-    if cannot_provide or True:
-        # its lattice representation
-        boh = lattice_inv[cannot_provide]
+    def get_frame_recommender(self, true_capabilities):
+        """Get the FrameRecommender for the selected true_capabilities.
+        
+        Params:
+        - true_capabilities: an array of strings"""
+        return FrameRecommender(self, true_capabilities)
+
+class FrameRecommender(object):
+    """A class to recommend Frames, from a Lattice and true capabilities"""
+
+    def __init__(self, lattice_specification, true_capabilities):
+        """lattice is an instance of Lattice
+        
+        true_capabilities is an array of capabilities that are"""
+        # the LatticeSpecification that generated this recommender
+        self.specification = lattice_specification
+        self.true_capabilities = true_capabilities
+    
+    def can_fulfil(self, frame_name, verbose=False, return_bool_only=True):
+        if verbose: print('requested:', frame_name)
+        # what are the abilities needed
+        wanted_cap = self.specification.context.intension([frame_name])
+        # what cannot be provided, but was asked for
+        cannot_provide_list = [cap for cap in wanted_cap if cap not in self.true_capabilities]
+        if verbose: print('\tthings required that cannot be provided: ', cannot_provide_list)
+        can_provide = False if cannot_provide_list else True
+        if return_bool_only:
+            return can_provide
+        else:
+            return can_provide, cannot_provide_list
+
+    def get_recommendation(self, frame_name, verbose=False):
+        # what are the abilities needed
+        wanted_cap = self.specification.context.intension([frame_name])
+        # what cannot be provided, but was asked for
+        cannot_provide_list = [cap for cap in wanted_cap if cap not in self.true_capabilities]
+        
+        # the lattice representation of the requested frame
+        anticoncept = self.specification.lattice_inv[frame_name,]
+        # the lattice representation of the missing capabilities
+        other = self.specification.lattice_inv[cannot_provide_list]
         # put together the constraints: we want things that:
-        # - do not require more (down from anticoncept)
-        # - do not require the things that we cannot provide (down from boh)
-        something = anticoncept.meet(boh)
-        print('legend: {set of things that can be done} <-> [list of abilities not required by all of them] <=> the upper neighbors (nearest) ')
-        print(something)
-    else:
-        print('I can fulfill', frame_name)
+        # - do not require more (down from anticoncept) (-->simpler<--)
+        # - do not require the things that we cannot provide (down from other) (-->feasible<--)
+        result_concept = anticoncept.meet(other)
+        if verbose:
+            print('nearest',result_concept.objects, 'between', result_concept.extent)
+        return Recommendation(result_concept, cannot_provide_list)
 
-def is_it_possible(frame_name):
-    # TODO delete, this is not working
-    # what are the abilities needed
-    wanted = context.intension([frame_name])
-    # its lattice representation
-    concept = lattice[frame_name,]
-    # what cannot be provided, but was asked for
-    can_provide = [cap for cap in wanted if cap in true_capabilities]
-    print('things required that can be provided: ', can_provide)
-    # its lattice representation
-    boh = lattice[can_provide]
-    # put together the constraints: we want things that:
-    # - do not require more (down from anticoncept)
-    # - do not require the things that we cannot provide (down from boh)
-    something = concept.join(boh)
-    # string representation is {extent} <-> [intent] <=> objects
-    print('legend: {set of things that can be done} <-> [list of abilities not required by all of them] <=> the upper neighbors (nearest) ')
-    print(something)
-
-
-# slower, stupid
-#get_candidates('Bringing')
-# faster, cool idea
-better('Bringing')
-# does not work because an upper group contains also the members of the descendants.
-# Going up to remove some required capabilities, we have in nodes elements that also have other requirements because are labeled down.
-# Instead working on the upside-down, the navigation is more natural because going down means to add things that are not required. So meeting with the required things that cannot be provided provides the answers
-#is_it_possible('Bringing')
-#get_candidates('Perception_active')
-better('Perception_active')
-#for frame in all_objects:
-#    better(frame)
+class Recommendation(object):
+    """A recommendation"""
+    def __init__(self, concept, reason_missing):
+        self.concept = concept
+        self.reason_missing = reason_missing
+        self.nearest = concept.objects
+        self.possible = concept.extent
